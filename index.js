bc632cbcf125b1ab4fa6c3371885746a556c31ee
Mark Roberts <marker2911@gmail.com>
2022-08-01 22:37:12 +0100

    Initialise the code and set up fetch request

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello folks")
+document.getElementById("idea-btn").addEventListener("click", function(){
+    console.log("clicked")
+    fetch("https://apis.scrimba.com/bored/api/activity")
+          .then(response => response.json())
+          .then(data => {
+              console.log(data)
+              document.getElementById("activity-text").innerHTML = data.activity
+          })
+})
+
