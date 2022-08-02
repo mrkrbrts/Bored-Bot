9251f5343d6e3c1e006f1919502eebcaa57baf23
Mark Roberts <marker2911@gmail.com>
2022-08-02 20:15:40 +0100

    Final changes!

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.getElementById("idea-btn").addEventListener("click", function(){
     console.log("clicked")
+    
+    changeGradient();
+
     fetch("https://apis.scrimba.com/bored/api/activity")
           .then(response => response.json())
           .then(data => {
@@ -8,3 +11,11 @@ document.getElementById("idea-btn").addEventListener("click", function(){
           })
 })
 
+function getRandomNumber(maxNum) {
+    return Math.floor(Math.random() * maxNum)
+}
+
+function changeGradient() {
+    // for some reason trying to change backgroundColor in js wasn't working!
+    document.body.style.backgroundImage = `linear-gradient(28deg, rgba(${getRandomNumber(255)},${getRandomNumber(255)},${getRandomNumber(255)},0.6) 0%, rgba(234,234,253,1) 50%, rgba(${getRandomNumber(255)},${getRandomNumber(255)},${getRandomNumber(255)},0.6) 100%)`
+}
